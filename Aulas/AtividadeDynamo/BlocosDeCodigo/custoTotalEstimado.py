1450630506f62ffde6b4ce509950ffb0d3427267
Bruno Leão <leaodebrito@icloud.com>
2022-10-22 15:19:13 -0300

    atualização do código ara cálculo do custo totla

--- Aulas/AtividadeDynamo/BlocosDeCodigo/custoTotalEstimado.py
+++ Aulas/AtividadeDynamo/BlocosDeCodigo/custoTotalEstimado.py
@@ -1,13 +1,15 @@
 area_de_forma = IN[0]
 volume_de_concreto = IN[1]
-volume_de_madeira = IN[2]
+peso_de_aco = IN[2]
+volume_de_madeira = IN[3]
 
-custo_forma = IN[3]
-custo_concreto = IN[4]
-custo_madeira = IN[5]
+custo_forma = IN[4]
+custo_concreto = IN[5]
+custo_madeira = IN[6]
+custo_aco = IN[7]
 
 #calculo do custo total
-custo_total = (area_de_forma * custo_forma) + (volume_de_concreto * custo_concreto) + (custo_madeira * volume_de_madeira)
+custo_total = (area_de_forma * custo_forma) + (volume_de_concreto * custo_concreto) + (custo_madeira * volume_de_madeira) + (peso_de_aco * custo_aco)
 
 
 #Atribuição a variável de saída
