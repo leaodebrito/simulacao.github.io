0173b6f1e5e5d3ac57acf4f0209e614b281a7d1f
Bruno Leão <leaodebrito@icloud.com>
2022-10-22 15:16:13 -0300

    atualização do código ara cálculo do custo totla

--- Aulas/AtividadeDynamo/BlocosDeCodigo/custoTotalEstimado.py
+++ Aulas/AtividadeDynamo/BlocosDeCodigo/custoTotalEstimado.py
@@ -1,13 +1,10 @@
-# TODO: inserir código na aula
-# TODO: Testar código no dynamo
+area_de_forma = IN[0]
+volume_de_concreto = IN[1]
+volume_de_madeira = IN[2]
 
-area_de_forma = 0
-volume_de_concreto = 0
-volume_de_madeira = 0
-
-custo_forma = 0
-custo_concreto = 0
-custo_madeira = 0
+custo_forma = IN[3]
+custo_concreto = IN[4]
+custo_madeira = IN[5]
 
 #calculo do custo total
 custo_total = (area_de_forma * custo_forma) + (volume_de_concreto * custo_concreto) + (custo_madeira * volume_de_madeira)
--- Aulas/AtividadeDynamo/BlocosDeCodigo/quantAreaDeForma.py
+++ Aulas/AtividadeDynamo/BlocosDeCodigo/quantAreaDeForma.py
@@ -1,5 +1,3 @@
-#TODO: Inserir código na explicação da atividade
-
 comprimento_viga = IN[0]
 base_viga = IN[1]
 altura_viga = IN[2]
