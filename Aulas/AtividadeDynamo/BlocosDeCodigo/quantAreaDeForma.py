7a8fc5c45a8ce9448502ba13d56d9a4c599a057e
Bruno Leão <leaodebrito@icloud.com>
2022-10-22 14:25:16 -0300

    cálculo de formas para pilares e vigas

--- Aulas/AtividadeDynamo/BlocosDeCodigo/quantAreaDeForma.py
+++ Aulas/AtividadeDynamo/BlocosDeCodigo/quantAreaDeForma.py
@@ -7,14 +7,32 @@ altura_viga = IN[2]
 comprimento_pilar = IN[3]
 lado_pilar = IN[4]
 
+quantidade_de_pilares = IN[5]
+quantidade_de_vigas = IN[6]
+
+material_pilares = IN[7]
+material_viga = IN[8]
+
+#área de
+
 #Calculo de forma das vigas
-forma_viga = (base_viga + (altura_viga * 2)) * comprimento_viga
+if material_viga == 1:
+    forma_viga = ((base_viga + (altura_viga * 2)) * comprimento_viga) * quantidade_de_vigas
+else:
+    forma_viga = 0
 
 #calculo de forma dos pilares
-forma_pilar = (lado_pilar * 4) * comprimento_pilar
+if material_pilares == 1:
+    forma_pilar = ((lado_pilar * 4) * comprimento_pilar) * quantidade_de_pilares
+else:
+    forma_pilar = 0
 
 #quantidade total de formas
 total_forma = forma_pilar + forma_viga
+
+
+
+
 
 
 OUT = total_forma
